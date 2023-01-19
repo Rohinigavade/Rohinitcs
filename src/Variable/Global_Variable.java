d6c0b8959c09e966960c63811886d72a2e6ee043
Admin <Admin@DESKTOP-5PKR16D>
2023-01-19 21:57:20 +0530

    My first project on github

---
+++ src/Variable/Global_Variable.java
@@ -0,0 +1,15 @@
+package Variable;
+
+public class Global_Variable {
+	static int i= 10; // static global variable 
+	int j=50;// global variable 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(i);
+		Global_Variable e=new Global_Variable();
+		System.out.println(e.j);
+
+	}
+
+}
---
+++ src/Variable/Local_Variable.java
@@ -0,0 +1,11 @@
+package Variable;
+
+public class Local_Variable { // class body open
+
+	public static void main(String[] args) { // main method body open
+		int i=50; // local variable 
+		System.out.println(i);
+
+	} // main method body close
+
+}// class body close 
